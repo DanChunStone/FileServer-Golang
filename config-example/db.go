f21f0422eb8fe3293963f93705c987d667686be8
DanChunStone <1912504224@qq.com>
2019-07-19 11:18:31 +0800

    add the config-example files

---
+++ config-example/db.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	DriverName     = "mysql"
+	DataSourceName = "root:root@tcp(localhost:3306)/fileserver?charset=utf8"
+)
---
+++ config-example/redis.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	RedisHost = "localhost:port"
+	RedisPass = ""
+)
